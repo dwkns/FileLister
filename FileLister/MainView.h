eff0f62d7d1453c3779944357ca69bee16cd9286
dwkns <dwkns@me.com>
2014-04-11 21:08:25 +0100

    Improvements to layout.

---
+++ FileLister/MainView.h
@@ -0,0 +1,13 @@
+//
+//  MainView.h
+//  FileLister
+//
+//  Created by dazza on 11/04/2014.
+//  Copyright (c) 2014 dazza. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainView : NSView
+
+@end
---
+++ FileLister/MainView.m
@@ -0,0 +1,29 @@
+//
+//  MainView.m
+//  FileLister
+//
+//  Created by dazza on 11/04/2014.
+//  Copyright (c) 2014 dazza. All rights reserved.
+//
+
+#import "MainView.h"
+
+@implementation MainView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
